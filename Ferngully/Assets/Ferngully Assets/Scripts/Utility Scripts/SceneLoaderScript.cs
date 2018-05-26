57334c9c45f63fcf6b29d8a7a2a21d1ada73d048
SanttuA <santtualatalo@gmail.com>
2018-05-26 09:12:58 +0300

    Added a simple scene loader script.

---
+++ Ferngully/Assets/Ferngully Assets/Scripts/Utility Scripts/SceneLoaderScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderScript : MonoBehaviour {
+
+    //Static instance of SceneLoader which allows it to be accessed by any other script.
+    public static SceneLoaderScript instance = null;
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
+
+    /// <summary>
+    /// Reloads the currently active scene which basically acts as a reset.
+    /// </summary>
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    /// <summary>
+    /// Loads the main menu scene which should be the first scene loaded when the game starts.
+    /// </summary>
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetSceneByBuildIndex(0).buildIndex);
+    }
+
+    /// <summary>
+    /// Loads the desired scene based on its name.
+    /// </summary>
+    /// <param name="sceneName"></param>
+    public void LoadSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(SceneManager.GetSceneByName(sceneName).buildIndex);
+    }
+}
