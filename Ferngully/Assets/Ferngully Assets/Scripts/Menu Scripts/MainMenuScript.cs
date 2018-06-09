5dd98ec9fe15297bfcf194be80a06c6cad8b07f9
SanttuA <santtualatalo@gmail.com>
2018-06-09 08:45:53 +0300

    Added a basic main menu

---
+++ Ferngully/Assets/Ferngully Assets/Scripts/Menu Scripts/MainMenuScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour {
+
+    public GameObject optionsPanel; //options panel in main menu. Set in editor
+    public string firstRoomName;    //first playable level player enters when hitting start game
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void HandleStartButton()
+    {
+        //start game from the first playable scene which should be at index 1 (main menu at 0)
+        SceneLoaderScript.instance.LoadSceneByName(firstRoomName);
+    }
+
+    public void HandleContinueButton()
+    {
+        //load the correct scene
+    }
+
+    public void HandleOptionsButton()
+    {
+        //open options menu panel and close main panel
+        optionsPanel.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    public void HandleExitButton()
+    {
+        //closes the application
+        Application.Quit();
+    }
+
+    private void OnEnable()
+    {
+        //set first main panel element to be active for navigation
+    }
+}
---
+++ Ferngully/Assets/Ferngully Assets/Scripts/Menu Scripts/OptionsMenuScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenuScript : MonoBehaviour {
+
+    public GameObject mainPanel;    //the main menu. Set in editor
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void HandleBackButton()
+    {
+        //open main menu panel and close options panel
+        mainPanel.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        //set first options panel element to be active for navigation
+    }
+}
